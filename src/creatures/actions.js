cf0d03a6559309eac05f27eb5209c2b79c17268d
lolotrgeek <lolotrgeek@gmail.com>
2021-03-18 10:18:39 -0500

    separate actions, refactor methods

---
+++ src/creatures/actions.js
@@ -0,0 +1,48 @@
+
+// Creature Action Space
+// every action has an energy cost, 
+// the cost will change depending on the environment 
+// i.e. movemvent in a high friction envrionment costs more energy
+
+class Actions {
+    constructor() {}
+
+    select(self, others) {
+        // select a mate by attractiveness
+        let potentials = others.filter(other => {
+            let attraction = Math.abs(self.attractions[0] - other.dna.genes[0])
+            // ignore un-attractive...
+            return attraction > self.fuzz ? true : false
+        })
+        // select the most attractive
+        let selection = Array.max(potentials)
+        return selection
+    }
+
+    move(self) {
+        
+    }
+
+    eat(self){
+
+    }
+
+    give(self, nearby) {
+        // give energy to nearby
+    }
+
+    sexual_reproduce(self, mate) {
+        let genes = self.dna.genes.concat(mate.dna.genes)
+        let childDNA = self.dna.crossover(genes)
+        return childDNA
+    }
+
+    asexual_reproduce(self) {
+        let childDNA = null
+        if (random(1) < 0.0005) childDNA = self.dna.copy()
+        return childDNA
+    }
+
+}
+
+module.exports = { Actions }
--- src/creatures/bloop.js
+++ src/creatures/bloop.js
@@ -1,45 +1,37 @@
-// The Nature of Code
-// Daniel Shiffman
-// http://natureofcode.com
-
-// Evolution EcoSystem
-
 // Creature class
 // Create a "bloop" creature
+// NOTE: a creature is an environment
+const { Actions } = require('./actions')
+
 class Bloop {
   constructor(dna_, health) {
-    this.address = null
-    this.actions = []
-    this.position = null // Location, gets from environment observations
-    this.health = health // Life timer
-    this.dna = dna_ // DNA
-    this.attractions = [Math.random()] // trait(s) this agent is attracted to
+    // features
+    this.health = health
+    this.dna = dna_
+    this.attractions = [Math.random()]
     this.phenotype = { r: 0, g: 0, b: Math.round(this.attractions[0] * 100) }
+    this.address = null
+
+    // observations
+    this.position = null
     this.mate = null
-    this.ate = null // index of food eaten
+    this.ate = null
+
+    // spaces
+    this.actions = new Actions()
     this.observations = []
   }
 
   spin() {
-    this.update()
+    if (this.observations.length > 0) {
+      //NOTE: observation space = network ping distance 
+    }
+    this.observations = []
+    this.health -= 0.2
   }
-
-  // TODO: every bloop needs it's own process/address
 
   observe(bloops, foods) {
     this.observations.push({ bloops, foods })
-    // TODO: send to Agent...
-  }
-
-  update() {
-    if (this.observations.length > 0) {
-
-      // let movement
-      // this.position.add(movement)
-    }
-    this.observations = []
-    // Death always looming
-    this.health -= 0.2
   }
 
   reset() {
@@ -47,58 +39,9 @@ class Bloop {
     this.mate = null
   }
 
-
-  move(x, y) {
-    this.position.x = this.position.x + x
-    this.position.y = this.position.y + y
-  }
-
-  eat(food) {
-    this.health += 100
-    return food
-  }
-
-  select(nearby) {
-    // select a mate by attractiveness
-    let potentials = nearby.filter(bloop => {
-      let attraction = Math.abs(this.attractions[0] - bloop.dna.genes[0])
-      // ignore any others that are not attractive...
-      let attracted = a => a > fuzz ? true : false
-      if (attracted(attraction)) return true
-      else return false
-    })
-    let selection
-    // if there is more than one potential mate...reproduce with the most attractive one
-    if (potentials.length > 1) selection = Array.max(potentials)
-    // or just pick the only one... 
-    else selection = potentials[0]
-    return selection
-  }
-
-  give(nearby) {
-    // give energy to nearby
-  }
-
-  reproduce(mate) {
-    // local sexual reproduction
-    let genes = this.dna.genes.concat(mate.dna.genes)
-    let childDNA = this.dna.crossover(genes)
-    return childDNA
-  }
-
-  a_reproduce() {
-    // asexual reproduction
-    if (random(1) < 0.0005) {
-      let childDNA = this.dna.copy()
-      return childDNA
-    } else return null
-  }
   dead() {
-    if (this.health < 0.0) {
-      return true
-    } else {
-      return false
-    }
+    if (this.health < 0.0) return true
+    else return false
   }
 }
 
